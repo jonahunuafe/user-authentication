451438d29f2f3ddee9830b5c0cb2e8de1ec0e2d4
Jonah Unuafe <fejisfejiro@gmail.com>
2024-02-29 15:37:56 +0100

    added ErrorPage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-import Signup from "./components/Signup";
 import MainNavigation from "./components/MainNavigation";
 
 function App() {
-  return (
-    <>
-      <main>
-        <MainNavigation title="Already have an account?">
-          <a href="http://">Sign in</a>
-        </MainNavigation>
-        <Signup />
-      </main>
-    </>
+  return(
+    <MainNavigation />
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,9 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import RootLayout from './pages/Root';
+import ErrorPage from './pages/ErrorPage';
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {path: "/", element: <RootLayout />, errorElement: <ErrorPage />}
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
   </React.StrictMode>
 );
