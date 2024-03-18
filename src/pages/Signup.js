008bef6d28e2f4bba682600706b5ccf506dfcdfb
Jonah Unuafe <fejisfejiro@gmail.com>
2024-03-18 13:29:41 +0100

    styled pages

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -4,9 +4,7 @@ import Button from "../components/Button";
 import Input from "../components/Input";
 
 
-
 function Signup() {
-    
 
     function handleSubmit(event) {
         event.preventDefault();
@@ -15,22 +13,15 @@ function Signup() {
 
     return (
         <form onSubmit={handleSubmit}>
-            <div>
-                <h2>Create an account</h2>
-                <Input 
-                    label="First Name" 
-                    id="first-name" 
-                    type="text" 
-                />
-                <Input label="Last Name" id="last-name" type="text" />
-                <Input label="Email" id="email" type="email" />
-                <Input label="Password" id="password" type="password" />
-            </div>
-            <div>
-                <Button>Sign Up</Button>
-                <p>By clicking "Sign up" you agree to our <a href="http://">Terms & Privacy Policy</a></p>
-                <Button>Already have an account? <Link to="/login">Log in</Link></Button>
-            </div>
+            <h2>Create an account</h2>
+            <Input label="First Name" id="first-name" type="text" />
+            <Input label="Last Name" id="last-name" type="text" />
+            <Input label="Email" id="email" type="email" />
+            <Input label="Password" id="password" type="password" />
+    
+            <Button>Sign Up</Button>
+            <p>By clicking "Sign up" you agree to our <a href="http://">Terms & Privacy Policy</a></p>
+            <Button>Already have an account? <Link to="/login">Log in</Link></Button>  
         </form>
     );
 }
