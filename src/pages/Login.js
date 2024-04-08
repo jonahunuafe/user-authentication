f3735db6a33886e78f8ee2a4869638a94c6c14f9
Jonah Unuafe <fejisfejiro@gmail.com>
2024-04-08 13:45:56 +0100

    refactor Login component

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -12,13 +12,13 @@ function Login() {
     return (
         <>
             <MainNavigation />
-            <h2 className="login-h2">Log in</h2>
             <div className="container">
+                <h2 className="login-h2">Log in</h2>
                 <form onSubmit={handleSubmit}>
                     <Input label="Email" id="email" type="email" />
                     <Input label="password" id="passord" type="password" />
-                    <p><Link to="/login/passwordreset">Forgot your password?</Link></p>
                     <button className="loginBtn">Log in</button>
+                    <p><Link to="/login/passwordreset">Forgot your password?</Link></p>
                 </form>
                 <p>Don't have an account yet? <Link to="/">Sign up</Link></p>
             </div>
